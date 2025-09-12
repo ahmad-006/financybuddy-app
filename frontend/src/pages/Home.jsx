f23e1d08d704f6cacd7e15efb0c64fc26441f323
Ahmad Aamir <ahmadaamir1806@gmail.com>
2025-09-12 22:54:07 +0500

    fix(import): correct CustomerReviews path casing

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,8 +2,7 @@ import { useNavigate } from "react-router-dom";
 import Features from "../components/homepage/Features";
 import HeroSection from "../components/homepage/HeroSection";
 import LastSection from "../components/homepage/LastSection";
-import CustomerReviews from "../components/homepage/customerReviews";
-import Footer from "../components/homepage/Footer";
+import CustomerReviews from "../components/homepage/CustomerReviews";
 
 function Home() {
   const navigate = useNavigate();
