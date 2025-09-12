db1caaa2bb522b1fc64da9b9784c23d4e2bc900d
Ahmad Aamir <ahmadaamir1806@gmail.com>
2025-09-12 23:14:09 +0500

    feat: Prepare for deployment and improve UI

--- frontend/src/pages/AppLayout.jsx
+++ frontend/src/pages/AppLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar/Navbar";
+import Navbar from "../components/navbar/Navbar";
 import AIChat from "../components/homepage/ChatIcon";
 import Footer from "@/components/homepage/Footer";
 import ScrollToTop from "../components/ScrollToTop";
